da0663fc3d8e6f03565b9f28004fcbb76152c910
victorlee0911 <victorlee0911@gmail.com>
2019-03-23 11:14:51 -0700

    hkjhkjh

---
+++ index.js
@@ -0,0 +1,7 @@
+var express = require('express');
+var app = express();
+
+app.get('/', function(req,res,) {
+    res.send("Hello World");
+})
+app.listen(3000, ()=> console.log('App started on port 3000'));
